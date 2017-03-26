2e3596b624752b99a22e8eb55ef560730832cd12
Ryan Kulp <ryanckulp@gmail.com>
2017-03-26 10:06:30 -0400

    project client_id generator

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,13 @@
 class Project < ActiveRecord::Base
   belongs_to :user
 
+  before_create :generate_client_id
+
+  def generate_client_id
+    self.client_id = loop do
+      random = SecureRandom.hex(10)
+      break random unless Project.where(client_id: random).present?
+    end
+  end
+
 end
