e9d994f75819a83ccc4e9a6d1c21d8412ea9601c
Ryan Kulp <ryanckulp@gmail.com>
2017-03-26 10:04:47 -0400

    public facing id for event api calls

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170326133130) do
+ActiveRecord::Schema.define(version: 20170326140014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 20170326133130) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.text     "avatar_url"
+    t.string   "client_id"
     t.index ["user_id"], name: "index_projects_on_user_id", using: :btree
   end
 
