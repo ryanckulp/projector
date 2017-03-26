affb905cebdbfb43e757dc5899968037c8398fdb
Ryan Kulp <ryanckulp@gmail.com>
2017-03-26 10:06:06 -0400

    send new users directly to create a new project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
 		@user = current_user if current_user
 	end
 
-	# def after_sign_in_path_for(resource)
-	# 	(if something true) ? send_user_here : send_user_elsewhere
-	# end
+	def after_sign_in_path_for(resource)
+		current_user.projects.count.zero? ? new_project_path : projects_path
+	end
 
 end
