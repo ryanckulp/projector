f89d84dabe4236c9493c06cb90b3788e14eedd50
Ryan Kulp <ryanckulp@gmail.com>
2017-03-29 00:11:51 -0400

    perform product hunt lookup during creation

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,12 +3,34 @@ class Project < ActiveRecord::Base
   has_many :events, dependent: :destroy
 
   before_create :generate_client_id
+  after_create :lookup_product_hunt_votes
 
   def generate_client_id
     self.client_id = loop do
       random = SecureRandom.hex(10)
       break random unless Project.where(client_id: random).present?
     end
+  end
+
+  def lookup_product_hunt_votes
+    self.product_hunt_votes = product_hunt_vote_count
+    self.save!
+  end
+
+  def product_hunt_slug
+    self.product_hunt_url.split("/posts/")[1]
+  end
+
+  def product_hunt_vote_count
+    resp = Curl.get("https://api.producthunt.com/v1/posts/all?search[slug]=#{product_hunt_slug}") do |curl|
+      curl.headers['Authorization'] = "Bearer #{ENV['PH_ACCESS_TOKEN']}"
+    end
+
+    return unless resp.status.include?('20')
+
+    data = JSON.parse(resp.body)
+    post = data['posts'][0]
+    post['votes_count']
   end
 
 end
