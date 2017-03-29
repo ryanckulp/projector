c4062265e207464bc69cf515c1551ec67a0bfe80
Ryan Kulp <ryanckulp@gmail.com>
2017-03-29 00:12:04 -0400

    convenience method for user product hunt scores

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ActiveRecord::Base
   has_many :projects, dependent: :destroy
   has_many :events, through: :projects
   has_many :suggestions, dependent: :destroy
+
+  def product_hunt_votes
+    projects.map(&:product_hunt_votes).inject(0, &:+)
+  end
 end
