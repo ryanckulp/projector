2a95d7b1feafb63e6e75d0a1575f077722866962
Ryan Kulp <ryanckulp@gmail.com>
2017-03-29 00:11:10 -0400

    instructions for implementing events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,20 @@
-class EventsController
+class EventsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_project, only: [:configure]
   before_action :set_event, only: [:destroy]
 
   def destroy
   end
 
+  def configure
+  end
+
 
   private
+
+    def set_project
+      @project = current_user.projects.find(params[:id])
+    end
 
     def set_event
       @event = current_user.events.find(params[:id])
